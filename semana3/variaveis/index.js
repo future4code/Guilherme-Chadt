8420a97a75428dd134d7ea55f933d3642a9acbdb
soch-ctrl <chadt7oficial@gmail.com>
2020-09-28 17:08:42 -0300

    Exercícios JavaScript

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,81 @@
+// a = 10
+// b = 10
+
+/*console.log(b)
+10*/
+
+/*b = 5
+console.log(a, b)
+10,5*/
+
+/*a = 10
+b = 20
+c = a
+b = c
+a = b
+console.log(a, b, c)
+ 10, 20, a */
+
+let nome
+console.log(typeof nome) 
+
+let idade 
+console.log(typeof idade)
+
+// Consta undefined em ambas, pois não foram definidos valores durante sua declaração.//
+
+//Nome
+nome = prompt ("Qual seu nome?")
+alert("Olá!!", nome)
+
+//Idade
+idade = prompt("Qual sua idade?")
+console.log("Olá", nome, "você tem", idade ,"anos")
+
+//Endereço
+let endereco 
+endereco = prompt("Qual seu endereço?")
+console.log(endereco)
+
+//Cor favorita
+let cor
+cor = prompt("Qual sua cor favorita")
+console.log(cor)
+
+//Série favorita
+let seriefavorita
+seriefavorita = prompt("Qual sua série favorita?")
+console.log(seriefavorita)
+
+//Se tem filhos
+let filhos
+filhos = prompt("Tem filhos?")
+console.log(filhos)
+
+//Sistema operacional preferido
+let sistemaoperacional
+sistemaoperacional = prompt("Android ou IOS?")
+console.log(sistemaoperacional)
+
+//Comidas favoritas
+let comidasfavoritas = ["Macarrão", "Lasanha", "Pizza", "Lanche", "Frutas"]
+console.log(comidasfavoritas)
+
+//Imprimindo itens em lista
+
+console.log("Essas são minhas comidas favoritas:",comidasfavoritas[0],comidasfavoritas[1],
+comidasfavoritas[2],
+comidasfavoritas[4])
+
+//Perguntando ao usuário
+ comidasfavoritas = prompt("Qual sua comida favorita?")
+
+console.log(comidasfavoritas)
+
+//Perguntas
+
+let perguntas = ["Qual cor da sua roupa no momento?","Qual a cor dos seus olhos?","Você está de bom humor?"]
+console.log(perguntas)
+
+
+
