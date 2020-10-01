b9acbce889d15362dd39514381be483571c14671
soch-ctrl <chadt7oficial@gmail.com>
2020-10-01 16:50:45 -0300

    Exercício D incompleto Desafio 2 a fazer

---
+++ semana3/loops/index.js
@@ -0,0 +1,77 @@
+// OI!
+/*Exercício 1
+let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)
+
+Ao meu ver, o programa está dando o dobro do número.
+*/
+ 
+/* Exercício 2
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}
+
+ a)Imprime os números maiores que 18 
+ b)console.log([i])
+ */
+
+ /* Desafio 1
+
+ const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+let quantidadeAtual = 0
+while(quantidadeAtual < quantidadeTotal){
+  let linha = ""
+  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    linha += "0"
+  }
+  console.log(linha)
+  quantidadeAtual++
+} 
+Imprime o número 0 x vezes em relação ao número digitado pelo usuário
+*/
+
+/* Exercício 3
+a)
+ const arrayOriginal=[10,22,34,45,51,65,78]
+     console.log(arrayOriginal);
+    
+b)
+ const arrayOriginal=[10,22,34,45,51,65,78]
+
+for(let i= 0 ; i < arrayOriginal.length; i++){
+    console.log(arrayOriginal[i]/10)
+}
+
+c)
+const arrayOriginal=[10,22,34,45,51,65,78]
+
+for(let i= 0 ; i < arrayOriginal.length; i++){
+    console.log(arrayOriginal[i] % 2 == 0)
+
+d)
+const arrayOriginal=[10,22,34,45,51,65,78];
+const arrayDoParaguai=[];
+
+for(let i= 0 ; i < arrayOriginal.length; i++){
+    if(arrayOriginal[i] % 2 === 0){
+      arrayDoParaguai.push(arrayOriginal[i])
+        console.log(arrayDoParaguai)
+
+    }
+    
+    
+    }
+    
+
+  
+  
+
+*/
+
+
