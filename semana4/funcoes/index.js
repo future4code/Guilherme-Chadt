4d4ac5c2fb8040dd5c3eea7166afa31eb5bed541
soch-ctrl <chadt7oficial@gmail.com>
2020-10-06 08:46:27 -0300

    Funcoes

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,106 @@
+// EXEMPLO 1 
+
+// function dizOi() {
+//     console.log("oi")
+// }
+
+// dizOi()
+
+
+// EXEMPLO 2
+
+// ------------------------------------------- EXEMPLO DE FUNCAO NOMEADA -----------------
+
+// const resultado1 = somaDoisNumeros(5, 6)
+// const resultado2 = somaDoisNumeros(10, 20)
+
+// console.log(resultado1)
+// console.log(resultado2)
+
+// function somaDoisNumeros(numero1, numero2) {
+//     const soma = numero1 + numero2
+//     return soma
+// }
+
+
+
+
+// ------------------------------------------- EXEMPLO DE FUNCAO NÃO NOMEADA -----------------
+
+
+// const somaDoisNumeros = function(numero1, numero2) {
+//     const soma = numero1 + numero2
+//     return soma
+// }
+    
+// const resultado1 = somaDoisNumeros(5, 6)
+// const resultado2 = somaDoisNumeros(10, 20)
+
+// console.log(resultado1)
+// console.log(resultado2)
+
+
+
+// ------------------------------------------- EXEMPLO DE ARROW FUNCTION (FUNCAO FLECHA) -----------------
+
+
+// const somaDoisNumeros = (numero1, numero2) => {
+//     const soma = numero1 + numero2
+//     return soma
+// }
+
+
+// const resultado1 = somaDoisNumeros(5, 6)
+// const resultado2 = somaDoisNumeros(10, 20)
+
+// console.log(resultado1)
+// console.log(resultado2)
+
+
+//Exercício 1
+//a)
+//2*5 e 10*5
+//b)
+//Náo aparece mensagem nenhuma
+
+//Exercício 2
+//a)
+/* Variável arrayDeNomes recebe os nomes.
+Função for cria 3 expressões que devem estar entre parênteses e separadas por ponto e vírgula.
+Variável recebe valor 0, enquanto i for menor que 2, incrementador soma mais um ao índice e imprime os dois primeiros nomes.*/
+//b)Seriam imprimidos os novos valores.
+
+//Exercício 3
+//Multiplica os números pares por eles mesmos e devolve seus valores no arrayFinal
+//nome da array = multiplyThemselves
+
+//Exercício 4
+/*a)
+
+function minhaBio(){
+    console.log("Eu sou o Guilherme, tenho 23 anos, resido em Sorocaba e sou estudante na Labenu")
+}
+
+minhaBio() */
+
+/*b
+
+let minhabio2=(nome, idade, endereco,  estudante)=>{
+    if(estudante===sim){
+        return true
+}else(estudante===não) 
+return false
+}
+const nome = prompt("Qual seu nome?")
+const idade = prompt("Qual sua idade?")
+const endereco = prompt("Qual sua cidade?") 
+const estudante = prompt("Você é estudante? Sou/não sou.")
+
+console.log(`Eu sou o ${nome}, tenho ${idade}, sou de ${endereco}, ${estudante} estudante.`)
+
+minhabio2()
+*/
+
+//Exercício 5
+//a)
+//b) 
